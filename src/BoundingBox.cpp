e02966de14d5859b0154c644a4d26bbb9b8222aa
stester <stester@localhost>
2017-02-21 12:03:33 +0000

    Added Bounding Box with no state or functionality

---
+++ src/BoundingBox.cpp
@@ -0,0 +1,18 @@
+/*
+ * BoundingBox.cpp
+ *
+ *  Created on: 21 Feb 2017
+ *      Author: stester
+ */
+
+#include "BoundingBox.h"
+
+BoundingBox::BoundingBox() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BoundingBox::~BoundingBox() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/BoundingBox.h
@@ -0,0 +1,17 @@
+/*
+ * BoundingBox.h
+ *
+ *  Created on: 21 Feb 2017
+ *      Author: stester
+ */
+
+#ifndef SRC_BOUNDINGBOX_H_
+#define SRC_BOUNDINGBOX_H_
+
+class BoundingBox {
+public:
+	BoundingBox();
+	virtual ~BoundingBox();
+};
+
+#endif /* SRC_BOUNDINGBOX_H_ */
